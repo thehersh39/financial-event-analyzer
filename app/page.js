c6246a3fab13a1ce02a75ed33f228b4f564af9fa
thehersh39 <sean@hockeystickventuresllc.com>
2025-07-29 14:34:02 -0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import FinancialEventAnalyzer from '../components/FinancialEventAnalyzer'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <FinancialEventAnalyzer />
+    </main>
+  )
+}
